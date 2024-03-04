7b6b2a61b6a7ff2cb17b6b8c06df45bf484413ec
Ilya Buzunov <ilia.buzunov@mail.ru>
2024-03-04 11:27:59 +0300

    анимации, прикрутил wav-файлы

--- MultiInstaller/MainWindow.xaml.cs
+++ MultiInstaller/MainWindow.xaml.cs
@@ -1,19 +1,15 @@
-﻿using System;
+﻿using ReactiveUI.Fody.Helpers;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Media;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Path = System.IO.Path;
+using System.Windows.Media.Animation;
 
 namespace MultiInstaller
 {
@@ -22,64 +18,119 @@ namespace MultiInstaller
     /// </summary>
     public partial class MainWindow : Window
     {
+        static int itterator = 0;
+        List<string> randData;
+        string soundsDir = Path.Combine(Directory.GetCurrentDirectory(), "Sounds");
+
         public MainWindow()
         {
             InitializeComponent();
 
-            if (!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Installers")))
-            {
-                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "Installers"));
-            }
+            List<string> data = 
+                ["Красндор", "Красндор", "Красндор", "Красндор", "Красндор", "Красндор", "Красндор", "Красндор",
+                 "Зелинзерин", "Зелинзерин", "Зелинзерин", "Зелинзерин", "Зелинзерин", "Зелинзерин", "Зелинзерин", "Зелинзерин",
+                 "Жёлтендуй", "Жёлтендуй", "Жёлтендуй", "Жёлтендуй", "Жёлтендуй", "Жёлтендуй", "Жёлтендуй", // Тут 7
+                 "Синевран", "Синевран", "Синевран", "Синевран", "Синевран", "Синевран", "Синевран", "Синевран"];
+
+            randData = Shuffle(data);
         }
 
-        private void onClickButtonGraphViewer(object sender, RoutedEventArgs e)
+        private async void onClickButtonStart(object sender, RoutedEventArgs e)
         {
-            if (File.Exists(Directory.GetCurrentDirectory() + @"\Installers\GraphViewerInstaller.exe"))
+            // Очищаем поле
+            textblock1.Background = new SolidColorBrush(Colors.Transparent);
+            textblock1.Text = null;
+            textblock2.Text = null;
+
+            if (itterator >= randData.Count)
             {
-                System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + @"\Installers\GraphViewerInstaller.exe");
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                player.SoundLocation = "Sounds/krasndor.wav";
+                player.Play();
+
+                //.mp3
+                await Task.Delay(5 * 1000);
+                App.Current.Shutdown();
+                return;
             }
+
+            // Запускаем анимацию
+            transform.BeginAnimation(RotateTransform.AngleProperty,
+                new DoubleAnimation
+                {
+                    From = 0,
+                    To = 360,
+                    Duration = TimeSpan.FromSeconds(1)
+                });
+
+            await Task.Delay(1 * 1000);
+
+            // Ставим новые значения
+            textblock1.Text = randData[itterator];
+            if (randData[itterator] == "Красндор")
+            {
+                textblock1.Foreground = new SolidColorBrush(Colors.White);
+                textblock1.Background = new SolidColorBrush(Colors.Red);
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                if (Directory.Exists(soundsDir))
+                    player.SoundLocation = Path.Combine(soundsDir, "krasndor.wav");
+                player.Play();
+            }
+            else if (randData[itterator] == "Зелинзерин")
+            {
+                textblock1.Foreground = new SolidColorBrush(Colors.White);
+                textblock1.Background = new SolidColorBrush(Colors.Green);
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                if (Directory.Exists(soundsDir))
+                    player.SoundLocation = Path.Combine(soundsDir, "zelinzerin.wav");
+                player.Play();
+            }
+            else if(randData[itterator] == "Жёлтендуй")
+            {
+                textblock1.Foreground = new SolidColorBrush(Colors.Black);
+                textblock1.Background = new SolidColorBrush(Colors.Yellow);
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                if (Directory.Exists(soundsDir))
+                    player.SoundLocation = Path.Combine(soundsDir, "jeltendyi.wav");
+                player.Play();
+            }
+            else if(randData[itterator] == "Синевран")
+            {
+                textblock1.Foreground = new SolidColorBrush(Colors.White);
+                textblock1.Background = new SolidColorBrush(Colors.Blue);
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                if (Directory.Exists(soundsDir))
+                    player.SoundLocation = Path.Combine(soundsDir, "sinevran.wav");
+                player.Play();
+            }
+            textblock1.Foreground = new SolidColorBrush(Colors.White);
+            itterator++;
+
+            textblock2.Text = $"Осталось: {randData.Count - itterator}";
         }
-        private void onClickButtonD14Black(object sender, RoutedEventArgs e)
+
+        public List<string> Shuffle(List<string> list)
         {
-            if (File.Exists(Directory.GetCurrentDirectory() + @"\Installers\D14BlackInstaller.exe"))
+            Random rand = new Random();
+
+            for (int i = list.Count - 1; i >= 1; i--)
             {
-                System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + @"\Installers\D14BlackInstaller.exe");
+                int j = rand.Next(i + 1);
+
+                var tmp = list[j];
+                list[j] = list[i];
+                list[i] = tmp;
             }
+
+            return list;
         }
-        private void onClickButtonSI25Black(object sender, RoutedEventArgs e)
-        {
-            if (File.Exists(Directory.GetCurrentDirectory() + @"\Installers\SI25BlackInstaller.exe"))
-            {
-                System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + @"\Installers\SI25BlackInstaller.exe");
-            }
-        }
-        private void onClickButtonCRSBlack(object sender, RoutedEventArgs e)
-        {
-            if (File.Exists(Directory.GetCurrentDirectory() + @"\Installers\CRSBlackInstaller.exe"))
-            {
-                System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + @"\Installers\CRSBlackInstaller.exe");
-            }
-        }
-        private void onClickButtonCRSWhite(object sender, RoutedEventArgs e)
-        {
-            if (File.Exists(Directory.GetCurrentDirectory() + @"\Installers\CRSWhiteOldInstaller.exe"))
-            {
-                System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + @"\Installers\CRSWhiteOldInstaller.exe");
-            }
-        }
-        private void onClickButtonD14(object sender, RoutedEventArgs e)
-        {
-            if (File.Exists(Directory.GetCurrentDirectory() + @"\Installers\D14Installer.exe"))
-            {
-                System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + @"\Installers\D14Installer.exe");
-            }
-        }
-        private void onClickButtonCRS(object sender, RoutedEventArgs e)
-        {
-            if (File.Exists(Directory.GetCurrentDirectory() + @"\Installers\CRSInstaller.exe"))
-            {
-                System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + @"\Installers\CRSInstaller.exe");
-            }
-        }
+
+        //SoundPlayer player = new SoundPlayer();
+        //player.Stream = Properties.Resources.Ding;
+        //player.Play();
     }
 }
